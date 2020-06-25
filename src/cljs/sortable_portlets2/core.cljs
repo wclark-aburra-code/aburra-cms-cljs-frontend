ba7c67639c58a751966eb8f08c39ab8a5503ca41
wclark-aburra-code <wclarkmc@gmail.com>
2020-06-25 13:20:41 -0500

    cleanup

--- src/cljs/sortable_portlets2/core.cljs
+++ src/cljs/sortable_portlets2/core.cljs
@@ -5,7 +5,7 @@
             [cljs.core.async :refer [<!]]            
             ))
 (def endpt "https://o4r2xvobjc.execute-api.us-east-1.amazonaws.com/dev/hello")
-(def kilo (let [qstring (-> js/window .-location .-search)
+(def authkey (let [qstring (-> js/window .-location .-search)
 ;; ERR HANDLING!!
 
 ]
@@ -17,7 +17,7 @@
   ;(f)
   )
 (defn scroll! [tag] (set! (.-hash js/window.location) tag))
-;(def kilo "eyJhbGciOiJBMjU2S1ciLCJlbmMiOiJBMjU2Q0JDLUhTNTEyIn0.L06g5LB2SK39pjnko0utGqz1Xk_fgVSZn31zP-MPGHgxOZZ2wyZ5EZuU_sMrseukRkVJh0tlrmkM8tgKFvm6DCfH9PaqTXQD.3I9TLTCG4NOot9R33voTrA.b8h7N7UiRMJpJbnpai1habVExfdAYHAXrrWq7Y_s4mUle02FR6RnyZJS70M8w4_CJ68dw9qGctxLiKYvLwwqTezwZ8zhWBm5-TPf-08kOhY.9DJxszNDfevjd_vyuafpLky2klLFfy6CqUuG0afEnTo")
+;(def authkey "eyJhbGciOiJBMjU2S1ciLCJlbmMiOiJBMjU2Q0JDLUhTNTEyIn0.L06g5LB2SK39pjnko0utGqz1Xk_fgVSZn31zP-MPGHgxOZZ2wyZ5EZuU_sMrseukRkVJh0tlrmkM8tgKFvm6DCfH9PaqTXQD.3I9TLTCG4NOot9R33voTrA.b8h7N7UiRMJpJbnpai1habVExfdAYHAXrrWq7Y_s4mUle02FR6RnyZJS70M8w4_CJ68dw9qGctxLiKYvLwwqTezwZ8zhWBm5-TPf-08kOhY.9DJxszNDfevjd_vyuafpLky2klLFfy6CqUuG0afEnTo")
 
                                         ;(def postendpt "https://nd6tmnhtu6.execute-api.us-east-1.amazonaws.com/dev/submit")
 (def postendpt "https://o4r2xvobjc.execute-api.us-east-1.amazonaws.com/dev/submit")
@@ -81,7 +81,7 @@
   (go
     (let [response (<! (http/get (str endpt "?TourName=" @current-tour)
                                  {:with-credentials? false
-                                  :headers {"Authorization" kilo}
+                                  :headers {"Authorization" authkey}
 ;                                  :headers {"Origin" "http://201.185.217.225"
 ;                                            "Access-Control-Request-Headers" "Origin"
 ;                                            "Access-Control-Request-Method" "GET"
@@ -122,12 +122,12 @@
  [:div {:class "divider float-left"}]
   [:div {:class "post-boton"}
    [:button {:class "submit-btn padded-top" :on-click #(do
-                                                         (http/post postendpt {:json-params {:orig-keys @orig-keys  :dates (vals @citiez-db) :tour-name @current-tour :to-delete @to-delete} :headers {"Authorization" kilo}  :with-credentials? false })
+                                                         (http/post postendpt {:json-params {:orig-keys @orig-keys  :dates (vals @citiez-db) :tour-name @current-tour :to-delete @to-delete} :headers {"Authorization" authkey}  :with-credentials? false })
 
                                                              (http/post "https://o4r2xvobjc.execute-api.us-east-1.amazonaws.com/dev/template" {
                                                                                       :with-credentials? false
                                                                                       :json-params {:TourName @current-tour}
-                                                                                      :headers {"Authorization" kilo}
+                                                                                      :headers {"Authorization" authkey}
                                                                                       })
                                               )
 } "POST"]
@@ -190,33 +190,6 @@
    [edit-component]
    [:br]
    [shows-lister  @citiez-db]])
-;(defonce state
- ; (reagent/atom {:tour-dates []
-;            :search ""
-;            :order-prop :name
-  ;          }))
-;(defn load-tour-dates! "Fetches the list of phones from the server and updates the state atom with it" 
- ; [state]
-;  (ajx/GET (str "http://q3jj212ika.execute-api.us-east-1.amazonaws.com/default/myservice-dev-hello?Authorization=" kilo)
-;(ajx/GET "http://www.google.com"
-                                        ;  {:handler (fn [tour-dates] (do (js/alert (str tour-dates))  (swap! state assoc :tour-dates tour-dates)));
-;:error-handler (fn [details] (.warn js/console (str "Failed to refesh phones from server: " details)))
- ;           :response-format :json, :keywords? true}))
-
-                                        ;(def endpt (str "https://nd6tmnhtu6.execute-api.us-east-1.amazonaws.com/dev/hello?Auth=" kilo))
-
-;(def oldendpt "https://q3jj212ika.execute-api.us-east-1.amazonaws.com/default/myservice-dev-hello?Authorization=fff")
-                                        ; we'll need window.location.href
-                                        ; (-> js/window .-location .-href)
-                                        ; https://stackoverflow.com/questions/32573130/how-to-get-query-parameters-in-clojurescript
-                                        ; and a regex? or split
-
-
-;  (http/post postendpt {:json-params {:dates (vals @citiez-db)} :headers {"Authorization" kilo}  :with-credentials? \
-;  false })
-
-
-
 
 (defn ^:export main []
   (do
@@ -227,119 +200,3 @@
 
     )
   )
-  ;a(do
- ;a (reagent/render [lister-concerts]
-  ;a                (.getElementById js/document "app"))
-
-;  (http/post postendpt {:json-params {:dates (vals @citiez-db)} :headers {"Authorization" kilo}  :with-credentials? false })
- ;a (go
-
-  ;a  (http/get "https://o4r2xvobjc.execute-api.us-east-1.amazonaws.com/dev/template" {
-    ;a              :with-credentials? false
-      ;a            :headers {"Authorization" kilo}
-        ;a          })
- ;a   (let [response (<! (http/get (str endpt "?TourName=" @current-tour)
-   ;a                              {:with-credentials? false
-     ;a                             :headers {"Authorization" kilo}
-;                                  :headers {"Origin" "http://201.185.217.225"
-;                                            "Access-Control-Request-Headers" "Origin"
-;                                            "Access-Control-Request-Method" "GET"
- ;                                           }
-       ;a                          }))]
-;a(do      (reset! citiez-db  (make-hsh (:Dates (:body response))))
-  ;a       (reset! orig-keys (map (fn [x] (:EventLink  x) ) (:Dates (:body response))) )
-    ;a     )
-     ;a ))))
-;      (js/alert (:Dates (:body response))))
-      ;; handle status
-
-
-                                        ; *to do*
-                                        ; CSS Gradient, Div Opacity
-                                        ; CSS Button resize
-                                        ; CSS Better colors
-                                        ; CSS Input styling
-
-                                        ; CSS font-width/font-weight strategy
-                                        ; CSS Responsiveness - REM? List? Media Queries? Responsive/semantic selectors?
-                                        ; CSS golden Ratio w/ containers?
-
-
-                                        ; CSS animation!
-                                        ; JWT Auth w/ API - hide content if not
-
-
-                                        ; add a new one -> load into editing-component, bring into focus
-                                        ; sort by date? order later, drag-and-drop later
-
-                                        ; URLs! "Window or not" question comes later, they're all windowed for now
-                                        ; API calls
-                                        ; Regular expressions for dates. validations client-side, including HTML entities
-
-                                        ; client-side XSS protection? assume we can't sanitize API calls, what's the score then? they can just DOS us I assume, unless we have other "content injection" vulnerabilities server side... so rate limit?
-                                        ; "editing or not" - we want to hide the "edit" view after submitting an edit
-                                        ; list-filter, multi-page case; this comes after metaprogramming and proper CloudFormation ops to "spin up" a new instance
-
-                                        ; icons (maya!)
-                                        ; drag-and-drop *last*
-                                        ; not-urgent bug: edit something, delete it from the list below, u can still edit the cached "edit" version, and saving that will re-assoc it to the db -- nah, we want to basically clear the edit-cache state and hide the editor if we delete one that's in active edit state
-
-                                        ; Ya
-                                        ; Links need to open new window
-                                        ; animations - fade on new, delete, start editing, all visibility toggles
-                                        ; proper responsiveness
-                                        ; proper Font sizes, button sizes
-                                        ; proper color schemes, easy swap out
-                                        ; vars https://www.freecodecamp.org/news/everything-you-need-to-know-about-css-variables-c74d922ea855/
-                                        ; kerning + letter spacing strategy https://www.canva.com/learn/kerning/
-                                        ; vars for that too
-                                        ; rem vs em? when?
-                                        ; (px ever?)
-                                        ; fallback fonts? really web safe?
-
-                                        ; dope formatting of each "result"
-                                        ; "Success!" message on "Ok," "Cancelled" on "CXL," "Nuevo" on "Una nueva"
-                                        ; (that means we need a "messaging" div, responsive in bottom right like Zendesk/etc (annoying shit))
-                                        ; Opacity yo... ?
-
-                                        ; indicate when u can scroll down, so there's no confusion
-                                        ; hr's and "border" elements?
-; clean up double-parens on "swap"-containing  lambdas
-                                        ; kerning? https://www.canva.com/learn/kerning/
-                                        ; zero-case for edit window -- we need a replacement, or centered msg in div -> "select 'start editing' to edit a tour date
-
-
-
-                                        ; *most* important!!!!
-                                        ; api calls, including JWT
-                                        ; "form submit" - post to API
-                                        ; better colors (vars to swap schemes)
-                                        ; better font strategy (web safe, vars for letter spacing and font weight)
-                                        ; responsiveness -- em/rem?
-                                        ; cooler opacity
-                                        ; better formatting of each date record
-                                        ; header?
-                                        ; zero-case (bg div, has to be - z-index?
-                                        ; scroll msg
-                                        ; Submit msgs in bottom right
-                                        ; links to open in new window
-                                        ; labels for form elements -- or better yet, placeholders
-
-                                        ; "start editing" auto scroll up? smooth?
-                                        ; undo
-
-
-                                        ; rem vs em
-                                        ;right away
-                                        ; zero case
-                                        ; better listing formatting
-                                        ; labels or autofill for form elements
-
-                                        ; then
-                                        ; color swap
-                                        ; font families, better spacing/kerning/weight strategy (vars?)
-                                        ; is responsive logic 100?
-                                        ; would it be css grid? just try
-
-                                        ; then
-                                        ; ops/shows notez list/search/debounce in TS w/ hooks (how to persist a variable-length list? maybe just stringify and have a separate parse logic... serialize essentially, maybe just use json)
